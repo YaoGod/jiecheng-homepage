085572c67befdd87f5506d350bf84247e6e64108
[YaoGod] <[1023612402@qq.com]>
2017-08-14 09:54:23 +0800

    first commit

---
+++ app/app.js
@@ -0,0 +1,50 @@
+'use strict';
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    'ngRoute',
+    'ui.router',
+    'ui.bootstrap',
+    'myApp.home'
+])
+    .config(['$locationProvider', '$urlRouterProvider','$stateProvider', function($locationProvider, $urlRouterProvider,$stateProvider) {
+
+      $urlRouterProvider.when("", "/home")
+          .otherwise("/home");
+      $stateProvider
+          .state('home', {
+              url: '',
+              templateUrl: 'home/header.html',
+              controller: 'CarouselDemoCtrl'
+          })
+          /*  nested list with custom controller*/
+          .state('home.page', {
+              url: '/home',
+              templateUrl: 'home/home.html',
+              controller: 'homeCtrl'
+          })
+    }])
+    .controller('CarouselDemoCtrl', function ($scope ,$http) {
+        $scope.myInterval = 3500;
+        $scope.noWrapSlides = false;
+        $scope.active = 0;
+        var slides = $scope.slides = [];
+        var currIndex = 0;
+        $http.get('./assets/ad')
+            .success(function (data) {
+                data = data + '';
+                var exp = '/assets/ad/.*\.(jpg|png)(?=")';
+                var paths = [];
+                var i = 0;
+                do{
+                    paths[i]=data.match(exp)[0];
+                    data = data.replace(paths[i], '');
+                    slides.push({
+                        image: '.' + paths[i],
+                        id: currIndex++
+                    });
+                    i++;
+                }while(data.match(exp) !== null)
+
+            });
+    });
+;
---
+++ app/home/home.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ZhangYaoYao on 2017/8/12.
+ */
+angular.module('myApp.home', [])
+    .controller('homeCtrl',[function () {
+
+    }]);
